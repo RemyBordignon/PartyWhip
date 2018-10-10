73349f0c4d995254836738789435b3f8bedd8300
Remy Bordignon <gsscg13@gmail.com>
2018-10-10 16:13:11 +1100

    Improve Forms
    Remove redundency

--- boards/forms.py
+++ boards/forms.py
@@ -15,7 +15,7 @@ class PostForm(ModelForm):
             'event_date': DateInput(),
             'end_date': DateInput(),
         }
-        exclude = ('pub_date', 'winnerSelected',)
+        exclude = ('user', 'pub_date', 'winner_selected',)
 
 
 class BidForm(ModelForm):
